0220c2ab2035facc0ec6f2197f5b0ddca771d030
MarshallDoyle <44276900+MarshallDoyle@users.noreply.github.com>
2023-12-27 20:35:25 -0800

    This adds the name counter and the name cleaner from november 2023

---
+++ CompanyNameCounter.py
@@ -0,0 +1,32 @@
+import csv
+from collections import defaultdict
+
+# Read the data from the input CSV file with UTF-8 encoding
+with open('C:/Users/marsh/OneDrive/Desktop/Patent Tracking/cleaned_combined_assignee_September25.csv', 'r', encoding='utf-8') as infile:
+    reader = csv.reader(infile)
+    data = list(reader)  # Convert the reader object to a list to get the total number of rows
+    total_rows = len(data)
+    company_names = [row[3] for row in data]
+
+# Count the occurrences of each company name
+company_name_count = defaultdict(int)
+for index, name in enumerate(company_names, 1):  # Start enumeration from 1 for row numbers
+    # Calculate and print the current processing percentage
+    percentage = (index / total_rows) * 100
+    print(f"Processing row {index} of {total_rows} ({percentage:.2f}% completed)")
+    
+    # Remove commas, ampersands, and periods and convert to lowercase
+    cleaned_name = name.replace(',', '').replace('&', '').replace('.', '').lower()
+    
+    company_name_count[cleaned_name] += 1
+
+# Sort the company name counts by count in descending order
+sorted_company_name_count = sorted(company_name_count.items(), key=lambda x: x[1], reverse=True)
+
+# Write the sorted company name counts to a new CSV file with UTF-8 encoding
+with open('CompanyNameCount_September25.csv', 'w', newline='', encoding='utf-8') as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(['Company Name', 'Count'])  # Write the header
+    writer.writerows(sorted_company_name_count)
+
+print("Company name counts have been written to output_company_names.csv")
---
+++ Company_name_cleaner_november5.py
@@ -0,0 +1,312 @@
+import csv
+import unicodedata
+import re
+
+#remove the words at the beginning of the string 
+def standardize_abbreviations(name):
+    abbreviations = {
+        r"(^| )inc( |$)": " ",
+        r"(^| )corporation( |$)": " ",
+        r"(^| )ltd( |$)": " ",
+        r"(^| )co( |$)": " ",
+        r"(^| )company( |$)": " ",
+        r"(^| )llc( |$)": " ",
+        r"(^| )the( |$)": " ",
+        r"(^| )of( |$)": " ",
+        r"(^| )kaisha( |$)": " ",
+        r"(^| )kabushiki( |$)": " ",
+        r"(^| )limited( |$)": " ",
+        r"(^| )gmbh( |$)": " ",
+        r"(^| )and( |$)": " ",
+        r"(^| )ag( |$)": " ",
+        r"(^| )incorporated( |$)": " ",
+        r"(^| )corp( |$)": " ",
+        r"(^| )aktiengesellschaft( |$)": " ",
+        r"(^| )lp( |$)": " ",
+        r"(^| )sa( |$)": " ",
+        r"(^| )as( |$)": " ",
+        r"(^| )de( |$)": " ",
+        r"(^| )bv( |$)": " ",
+        r"(^| )kg( |$)": " ",
+        r"(^| )ab( |$)": " ",
+        r"(^| )spa( |$)": " ",
+        r"(^| )nv( |$)": " ",
+        r"(^| )holdings( |$)": " ",
+        r"(^| )licensing( |$)": " ",
+        r"(^| )i( |$)": " ",
+        r"(^| )ip( |$)": " ",
+        r"(^| )holding( |$)": " ",
+        r"(^| )pty( |$)": " ",
+        r"(^| )e( |$)": " ",
+        r"(^| )telefonaktiebolaget( |$)": " ",
+        r"(^| )du( |$)": " ",
+        r"(^| )a/s( |$)": " ",
+        r"(^| )koninklijke( |$)": " ",
+        r"(^| )oy( |$)": " ",
+        r"(^| )et( |$)": " ",
+        r"(^| )srl( |$)": " ",
+        r"(^| )for( |$)": " ",
+        r"(^| )(publ)( |$)": " ",
+        r"(^| )a( |$)": " ",
+        r"(^| )pte( |$)": " ",
+        r"(^| )plc( |$)": " ",
+        r"(^| )lm( |$)": " ",
+        r"(^| )se( |$)": " ",
+        r"(^| )societe( |$)": " ",
+        r"(^| )la( |$)": " ",
+        r"(^| )l( |$)": " ",
+        r"(^| )m( |$)": " ",
+        r"(^| )kk( |$)": " ",
+        r"(^| )des( |$)": " ",
+        r"(^| )r( |$)": " ",
+        r"(^| )und( |$)": " ",
+        r"(^| )c( |$)": " ",
+        r"(^| )gesellschaft( |$)": " ",
+        r"(^| )mbh( |$)": " ",
+        r"(^| )j( |$)": " ",
+        r"(^| )ii( |$)": " ",
+        r"(^| )w( |$)": " ",
+        r"(^| )der( |$)": " ",
+        r"(^| )s( |$)": " ",
+        r"(^| )d( |$)": " ",
+        r"(^| )b( |$)": " ",
+        r"(^| )sarl( |$)": " ",
+        r"(^| )anonyme( |$)": " ",
+        r"(^| )maschinenfabrik( |$)": " ",
+        r"(^| )aktiebolag( |$)": " ",
+        r"(^| )g( |$)": " ",
+        r"(^| )-( |$)": " ",
+        r"(^| )sl( |$)": " ",
+        r"(^| )k( |$)": " ",
+        r"(^| )kgaa( |$)": " ",
+        r"(^| )kommanditgesellschaft( |$)": " ",
+        r"(^| )p( |$)": " ",
+        r"(^| )t( |$)": " ",
+        r"(^| )aktien( |$)": " ",
+        r"(^| )ohg( |$)": " ",
+        r"(^| )v( |$)": " ",
+        r"(^| )llp( |$)": " ",
+        r"(^| )oyj( |$)": " ",
+        r"(^| )x( |$)": " ",
+        r"(^| )societe( |$)": " ",
+        r"(^| )bvba( |$)": " ",
+        r"(^| )n( |$)": " ",
+        r"(^| )anonyme( |$)": " ",
+        r"(^| )o( |$)": " ",
+        r"(^| )ltda( |$)": " ",
+        r"(^| )y( |$)": " ",
+        r"(^| )kabushikikaisha( |$)": " ",
+        r"(^| )incorporation( |$)": " ",
+        r"(^| )publ( |$)": " "
+    }
+    
+    for abbr, full in abbreviations.items():
+        name = re.sub(abbr, full, name)
+
+    return name.strip()
+
+def remove_company_suffix(name):
+    # Define suffix patterns and their conditions
+    suffixes = {
+        r"(^| )mfg( |$)": " manufacturing ",
+        r"(^| )tech( |$)": " technology ",
+        r"(^| )lab(s)?( |$)": " laboratories ",
+        r"(^| )intl( |$)": " international ",
+        r"(^| )inst( |$)": " institute ",
+        r"(^| )assn( |$)": " association ",
+    }
+
+    for suffix, replacement in suffixes.items():
+        name = re.sub(suffix, replacement, name)
+        
+    return name.strip()
+
+def remove_international_terms(name):
+    terms = [
+        " a g",
+        " l p",
+        " s a",
+        " a s",
+        " a b",
+        " b v",
+        " k g",
+        " n v",
+        " s p a",
+        " s a r l",
+        " s r l",
+        " k k",
+        " s l",
+        " c a"
+    ]
+    for term in terms:
+        if name.endswith(term):
+            name = name[:-len(term)]
+    return name.strip()
+
+def Light_abbreviations(name):
+    abbreviations = {
+        r"(^| )inc( |$)": " ",
+        r"(^| )corporation( |$)": " ",
+        r"(^| )ltd( |$)": " ",
+        r"(^| )co( |$)": " ",
+        r"(^| )company( |$)": " ",
+        r"(^| )llc( |$)": " ",
+        r"(^| )the( |$)": " ",
+        r"(^| )of( |$)": " ",
+        r"(^| )kaisha( |$)": " ",
+        r"(^| )kabushiki( |$)": " ",
+        r"(^| )limited( |$)": " ",
+        r"(^| )gmbh( |$)": " ",
+        r"(^| )and( |$)": " ",
+        r"(^| )ag( |$)": " ",
+        r"(^| )incorporated( |$)": " ",
+        r"(^| )corp( |$)": " ",
+        r"(^| )aktiengesellschaft( |$)": " ",
+        r"(^| )lp( |$)": " ",
+        r"(^| )sa( |$)": " ",
+        r"(^| )as( |$)": " ",
+        r"(^| )de( |$)": " ",
+        r"(^| )bv( |$)": " ",
+        r"(^| )kg( |$)": " ",
+        r"(^| )ab( |$)": " ",
+        r"(^| )spa( |$)": " ",
+        r"(^| )nv( |$)": " ",
+        r"(^| )holdings( |$)": " ",
+        r"(^| )licensing( |$)": " ",
+        r"(^| )ip( |$)": " ",
+        r"(^| )holding( |$)": " ",
+        r"(^| )pty( |$)": " ",
+        r"(^| )telefonaktiebolaget( |$)": " ",
+        r"(^| )du( |$)": " ",
+        r"(^| )koninklijke( |$)": " ",
+        r"(^| )oy( |$)": " ",
+        r"(^| )et( |$)": " ",
+        r"(^| )srl( |$)": " ",
+        r"(^| )for( |$)": " ",
+        r"(^| )(publ)( |$)": " ",
+        r"(^| )pte( |$)": " ",
+        r"(^| )plc( |$)": " ",
+        r"(^| )lm( |$)": " ",
+        r"(^| )se( |$)": " ",
+        r"(^| )societe( |$)": " ",
+        r"(^| )la( |$)": " ",
+        r"(^| )kk( |$)": " ",
+        r"(^| )des( |$)": " ",
+        r"(^| )und( |$)": " ",
+        r"(^| )gesellschaft( |$)": " ",
+        r"(^| )mbh( |$)": " ",
+        r"(^| )der( |$)": " ",
+        r"(^| )sarl( |$)": " ",
+        r"(^| )anonyme( |$)": " ",
+        r"(^| )maschinenfabrik( |$)": " ",
+        r"(^| )aktiebolag( |$)": " ",
+        r"(^| )sl( |$)": " ",
+        r"(^| )kgaa( |$)": " ",
+        r"(^| )kommanditgesellschaft( |$)": " ",
+        r"(^| )aktien( |$)": " ",
+        r"(^| )ohg( |$)": " ",
+        r"(^| )llp( |$)": " ",
+        r"(^| )oyj( |$)": " ",
+        r"(^| )societe( |$)": " ",
+        r"(^| )bvba( |$)": " ",
+        r"(^| )anonyme( |$)": " ",
+        r"(^| )ltda( |$)": " ",
+        r"(^| )kabushikikaisha( |$)": " ",
+        r"(^| )incorporation( |$)": " ",
+        r"(^| )publ( |$)": " "
+    }
+    
+    for abbr, full in abbreviations.items():
+        name = re.sub(abbr, full, name)
+
+    return name.strip()
+# Main function to clean and standardize company names
+
+def clean_name(name):
+    # Convert name to lowercase
+    name = name.lower()\
+    # Normalize non-ASCII characters to ASCII\
+    name = unicodedata.normalize('NFKD', name).encode('ASCII', 'ignore').decode()\
+    # Remove extra spaces from name\
+    name = ' '.join(name.split())\
+    # Retain only alphanumeric characters and certain punctuation\
+    name = ''.join(e for e in name if e.isalnum() or e in '&-() ')\
+    # Replace dashes with spaces\
+    name = name.replace("-", " ")\
+    # Replace & with spaces\
+    name = name.replace(" & ", " ")\
+    # Strip\
+    name = name.strip()\
+    # Standardize abbreviations\
+    name = Light_abbreviations(name)\
+    # Remove company suffixes\
+    name = remove_company_suffix(name)
+    #Removing non alphanumeric
+    name = remove_non_alphanumeric(name)
+    #rRemoving spaces
+    name = remove_spaces(name)
+    #Remove the international terms
+    name = remove_international_terms(name)
+    # Remove single letters that stand alone
+    #name = re.sub(r'\b[a-z]\b', '', name)
+    return name
+
+def remove_non_alphanumeric(name):
+    return re.sub(r'[^a-zA-Z0-9 ]', ' ', name)
+
+def remove_spaces(name):
+    return name.replace(" ", "")
+
+"""
+Column Letter to Index Reference:
+A: 0
+B: 1
+C: 2
+D: 3
+E: 4
+F: 5
+G: 6
+H: 7
+I: 8
+J: 9
+K: 10
+L: 11
+M: 12
+"""
+COLUMN_TO_READ = 1  
+COLUMN_TO_COPY_TO = 2
+NEW_COLUMN_TITLE = "assignee_cleaned"
+
+input_file = 'C:/Users/marsh/Patent Tracking/Uncleaned/capitaliq_cusip.csv'
+output_file = 'C:/Users/marsh/Patent Tracking/Cleaned/Cleaned_capital_cusip.csv'
+
+unique_names = []
+full_data = []
+
+with open(input_file, 'r', encoding='utf-8') as infile:
+    csv_reader = csv.reader(infile)
+    total_rows = sum(1 for row in csv.reader(open(input_file, 'r', encoding='utf-8')))
+    infile.seek(0)  # Reset the file pointer to the beginning
+    for i, row in enumerate(csv_reader, 1):
+        if len(row) >= COLUMN_TO_READ:
+            original_name = row[COLUMN_TO_READ]
+            cleaned_name = clean_name(original_name)
+
+            # Add cleaned name to unique_names list
+            unique_names.append(cleaned_name)
+
+            # Print the original company name and percentage completion
+            percentage_complete = (i / total_rows) * 100
+            print(f"Row {i} ({percentage_complete:.2f}% complete): {original_name}")
+
+            # Insert cleaned name to the specified column
+            row.insert(COLUMN_TO_COPY_TO, cleaned_name)
+            full_data.append(row)
+
+# Write the data to a new CSV file
+with open(output_file, 'w', newline='', encoding='utf-8') as outfile:
+    csv_writer = csv.writer(outfile)
+    for row in full_data:
+        csv_writer.writerow(row)
+
+print(f"Data with cleaned names as a new column has been saved to {output_file}")
